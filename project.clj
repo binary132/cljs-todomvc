a7fa7e326fdceec95615ae8c953f297c7b072f15
Bodie Solomon <binary132@gmail.com>
2014-10-27 20:11:22 -0400

    Working Om app

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject todomvc-om "0.1.0-SNAPSHOT"
+(defproject cljs-todomvc "0.1.0-SNAPSHOT"
   :description "A sample TodoMVC project for Clojurescript/Om"
   :url "http://synapse-garden.github.com/cljs-todomvc"
   :license {:name "Eclipse Public License"
@@ -6,14 +6,16 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2371"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [om "0.7.3"]
                  [jasmine-cljs "0.1.5"]]
   :plugins [[lein-cljsbuild "1.0.3"]]
   :cljsbuild {
-    :builds [{:id "mies-om-sample"
+    :builds [{:id "cljs-todomvc"
               :source-paths ["src"]
               :compiler {
-                :output-to "mies_om_sample.js"
+                :output-to "cljs_todomvc.js"
                 :output-dir "out"
+                :pretty-print false
                 :optimizations :none
                 :source-map true}}]}
   :source-paths ["src"])
